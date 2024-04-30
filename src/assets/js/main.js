806b029c630254b27bffb5d8b042cd36dd7bec79
willsondev <willsondev12@gmail.com>
2024-04-29 20:57:50 -0400

    cambios segunda revision

---
+++ src/assets/js/main.js
@@ -0,0 +1,4 @@
+ document.getElementById("submitButton").addEventListener("click", function() {
+        var email = document.getElementById("emailInput").value;
+        alert("¡Gracias por suscribirte! Te mantendremos actualizado sobre el desarrollo Front End en " + email);
+    });
